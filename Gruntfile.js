780b48b47126df776b76563e00b691a1c081d524
Dirk Voss <dirk.voss@aixigo.de>
2015-07-31 09:36:30 +0200

    added live reload feature

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+/*global module,__dirname,__filename */
+module.exports = function( grunt ) {
+   'use strict';
+
+   var serverPort = 8080;
+   var liveReloadPort = 30000 + serverPort;
+
+   grunt.initConfig( {
+      pkg: grunt.file.readJSON( 'package.json' ),
+      connect: {
+         options: {
+            port: serverPort,
+            livereload: liveReloadPort
+         },
+         default: {
+
+         },
+         keepalive: {
+            options: {
+               port: serverPort,
+               keepalive: true
+            }
+         }
+      },
+      watch: {
+         options: {
+            livereload: liveReloadPort,
+            debounceDelay: 50
+         },
+         Gruntfile: {
+            files: __filename,
+            reload: true
+         },
+         dist: {
+            files: [
+               'dist/**/*.*'
+            ]
+         }
+      }
+   } );
+
+   grunt.loadNpmTasks( 'grunt-contrib-connect' );
+   grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+   grunt.registerTask( 'build', [  ] );
+   grunt.registerTask( 'start', [ 'build', 'connect:default', 'watch' ] );
+   grunt.registerTask( 'start-no-watch', [ 'build', 'connect:keepalive' ] );
+   grunt.registerTask( 'default', [ 'build' ] );
+
+};
