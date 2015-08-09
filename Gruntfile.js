1ccd9870e1f9dd3ad7e54e6a5f1af28b19fca55d
Dirk Voss <dirk.voss@aixigo.de>
2015-08-09 16:23:10 +0200

    Replace material-design-lite with materializecss

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function( grunt ) {
          },
          dist: {
             files: [
-               'dist/**/*.*'
+               ['dist/**/*.*', 'index.html']
             ]
          }
       }
